c0f7d5292ce49ced098644d8912cd00da2588e79
Ronald Mintz <ron_mintz@post.harvard.edu>
2022-07-24 23:45:52 -0400

    connect to mongodb in the digitalocean style

--- dal.js
+++ dal.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb+srv://doadmin:62V8P097W1vsJO4L@mongodb-2cbf95e2.mongo.ondigitalocean.com/admin?tls=true&authSource=admin';
+const url = 'mongodb+srv://doadmin:62V8P097W1vsJO4L@mongodb-2cbf95e2.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=mongodb';
 let db = null;
 console.log(url);
 
